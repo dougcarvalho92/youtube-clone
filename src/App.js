083b888851613adeb9704d47eee94ac278033566
dougcarvalho92 <dscarvalho199@gmail.com>
2020-08-17 18:51:47 -0300

    Estrutura e pequenos designs

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { ThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core";
+import { red, blue } from "@material-ui/core/colors";
+import Home from "./pages/Home";
+
+
+const useStyles = makeStyles({
+  root: {},
+});
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = createMuiTheme({
+    spacing: 4,
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f44336",
+      },
+      secondary: {
+        main: "#3EA6FF",
+      },
+      background: {
+        default: darkMode ? "#232323" : "#FFF",
+        dark: darkMode ? "#181818" : "#f4f6f8",
+        paper: darkMode ? "#232323" : "#FFF",
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme} >
+      <Home darkMode={darkMode} setDarkMode={setDarkMode}/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,83 @@
+import { fade, makeStyles } from "@material-ui/core";
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+    backgroundColor: theme.palette.background.dark,
+  },
+  appBar: {
+    boxShadow: "none",
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  logo: {
+    height: 25,
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+    borderRight: "none",
+  },
+  menuIcon: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(6),
+  },
+  icons: {
+    paddingRight: theme.spacing(5),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  listItemText: {
+    fontSize: 14,
+  },
+  listItem: {
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  subheader: {
+    textTransform: "uppercase",
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default useStyles;
